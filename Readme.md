bb64490903573b542b169019ffc142fe40551c1e
DevWambugu <venusdata100@gmail.com>
2023-05-23 12:59:25 -0700

    sort out echo

--- Readme.md
+++
@@ -1,1 +0,0 @@
-Create a unix simple shell
--- main.h
+++ main.h
@@ -20,4 +20,17 @@ char **split_input(char *input);
 char *get_full_path(char *command);
 void write_path(char *full_path, const char *dir, size_t dir_len, const char *command, size_t command_len);
 void create_and_write_file(char **args);
+void print_env(void);
+void print_env(void)
+{
+	extern char **environ;
+	char **env = environ;
+
+	while (*env != NULL)
+	{
+		write(1, *env, strlen(*env));
+		write(1, "\n", 1);
+		env++;
+	}
+}
 #endif
